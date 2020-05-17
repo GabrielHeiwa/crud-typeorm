7bae895460f06f5de72246d0d26d3399df273146
gabrielheiwa <heiwa.gabriel@gmail.com>
2020-05-17 10:49:31 -0300

    add: try catch's in UserController.js

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -5,17 +5,39 @@ import { User } from "../entity/User";
 
 export default {
   async getAllUser(req: Request, res: Response) {
-    const user = await getRepository(User).find();
-    return res.json(user);
+    try {
+      const user = await getRepository(User).find();
+      return res.json({ message: "create operation success.", user });
+    } catch (err) {
+      return res.json({
+        message: "Get all users operation failed, try again.",
+        info: err,
+      });
+    }
   },
 
   async getUser(req: Request, res: Response) {
     const { user_id } = req.params;
-    const user = await getRepository(User).findOne(user_id);
-    res.json(user);
+    try {
+      const user = await getRepository(User).findOne(user_id);
+      return res.json({ message: "Get user operation succes.", user });
+    } catch (err) {
+      return res.json({
+        message: "Get user operation failed, try again.",
+        info: err,
+      });
+    }
   },
+
   async createUser(req: Request, res: Response) {
-    const user = await getRepository(User).save(req.body);
-    res.json(user);
+    try {
+      const user = await getRepository(User).save(req.body);
+      return res.json({ message: "Create user operation success.", user });
+    } catch (err) {
+      return res.json({
+        message: "Create user operation failed, try again.",
+        info: err,
+      });
+    }
   },
 };
