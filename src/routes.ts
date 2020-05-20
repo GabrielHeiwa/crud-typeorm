ef1c54f0fae7dd016a8d588cddd5df296b4d60e8
gabrielheiwa <heiwa.gabriel@gmail.com>
2020-05-20 17:10:20 -0300

    add: event controller in routes.js

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import CoursesController from "./controller/CoursesController";
+import EventController from './controller/EventsController';
 const routes = Router();
 
 routes.get("/courses", CoursesController.index);
@@ -7,5 +8,11 @@ routes.get("/courses/:courses_id", CoursesController.show);
 routes.post("/courses", CoursesController.create);
 routes.delete("/courses/:courses_id", CoursesController.delete);
 routes.put("/courses/:courses_id", CoursesController.update);
+
+routes.get("/event", CoursesController.index);
+routes.get("/event/:event_id", CoursesController.show);
+routes.post("/event", CoursesController.create);
+routes.delete("/event/:event_id", CoursesController.delete);
+routes.put("/event/:event_id", CoursesController.update);
 
 export default routes;
