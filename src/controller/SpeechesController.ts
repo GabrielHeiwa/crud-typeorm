52e8d180b9710d5c0557bcf51b019a91137319f0
gabrielheiwa <heiwa.gabriel@gmail.com>
2020-05-20 17:17:29 -0300

    Add: create and implemetation CRUD in speeches

---
+++ src/controller/SpeechesController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Speeches } from "../entity/Speeches";
+export default {
+  async create(req: Request, res: Response) {
+    try {
+      const speeche_created = await getRepository(Speeches).save(req.body);
+      res.status(201).json({
+        speeche_created,
+      });
+    } catch (error) {
+      res.status(400).json({
+        message: "create speeche operation failed, try again.",
+        info: error,
+      });
+    }
+  },
+
+  async index(req: Request, res: Response) {
+    try {
+      const speeches_all = await getRepository(Speeches).find();
+      return res.status(200).json({
+        message: "success operation of get all speeches.",
+        data: speeches_all,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: "Get all speeches operation failed, try again.",
+        info: error,
+      });
+    }
+  },
+
+  async show(req: Request, res: Response) {
+    const { speeches_id } = req.params;
+    try {
+      const speeches = await getRepository(Speeches).findOne(speeches_id);
+      return res.status(200).json({
+        message: "Get speeche operation success.",
+        data: speeches,
+      });
+    } catch (error) {
+      return res.status(401).json({
+        message: "get speeches operation failed, try agaain.",
+        info: error,
+      });
+    }
+  },
+
+  async update(req: Request, res: Response) {
+    const { speeches_id } = req.params;
+    try {
+      const speeches_update = await getRepository(Speeches).update(
+        speeches_id,
+        req.body
+      );
+      return res.status(200).json({
+        message: "Update operation success.",
+        data: speeches_update,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: "Update operation failed, try again.",
+        info: error,
+      });
+    }
+  },
+
+  async delete(req: Request, res: Response) {
+    const { speeches_id } = req.params;
+    try {
+      await getRepository(speeches).delete(speeches_id);
+      return res.sendStatus(200).json({message: "delete operation success."});
+    } catch (error) {
+      return res.status(400).json({
+        message: "Update operation failed, try again.",
+        info: error,
+      });
+    }
+  },
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import CoursesController from "./controller/CoursesController";
 import EventController from './controller/EventsController';
+import SpeechesController  from './controller/SpeechesController';
 const routes = Router();
 
 routes.get("/courses", CoursesController.index);
@@ -9,10 +10,15 @@ routes.post("/courses", CoursesController.create);
 routes.delete("/courses/:courses_id", CoursesController.delete);
 routes.put("/courses/:courses_id", CoursesController.update);
 
-routes.get("/event", CoursesController.index);
-routes.get("/event/:event_id", CoursesController.show);
-routes.post("/event", CoursesController.create);
-routes.delete("/event/:event_id", CoursesController.delete);
-routes.put("/event/:event_id", CoursesController.update);
+routes.get("/event", EventController.index);
+routes.post("/event", EventController.create);
+routes.delete("/event/:event_id", EventController.delete);
+routes.put("/event/:event_id", EventController.update);
+
+routes.get("/speeches", SpeechesController.index);
+routes.get("/speeches/:speeches_id", SpeechesController.show);
+routes.post("/speeches", SpeechesController.create);
+routes.delete("/speeches/:speeches_id", SpeechesController.delete);
+routes.put("/speeches/:speeches_id", SpeechesController.update);
 
 export default routes;
